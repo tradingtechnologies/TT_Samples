b5597c4a5bfc8dd98e241639f16ff94cfb5db66c
chirag-tt <chirag.kapadiya@tradingtechnologies.com>
2022-10-11 05:54:18 -0500

    Env check

--- TT_NET_CLIENT_SIDE/TTNETAPI_Sample_Console_TTUncovered_MarketCreation/Program.cs
+++ TT_NET_CLIENT_SIDE/TTNETAPI_Sample_Console_TTUncovered_MarketCreation/Program.cs
@@ -581,12 +581,12 @@ namespace TTNETAPI_Sample_Console_TTUncovered_MarketCreation
         {
             try
             {
+                //string appSecretKey = "f8710ecf-f581-9bbb-ff33-e71f294f09d4:aec599a1-c784-585f-af28-318047ced955";
                 string appSecretKey = "834ac713-e67f-49b0-845f-a22c6f61d0c4:22b22302-edf8-365f-2541-aebc28715def";
                 ServiceEnvironment environment = ServiceEnvironment.DevCert;
                 TTAPIOptions.SDKMode sdkMode = TTAPIOptions.SDKMode.Client;
 
-                Console.WriteLine("Enter environment[default: int_dev_cert]");
-                string env = Console.ReadLine();
+                Console.Write("Enter environment[options: ");
 
                 Dictionary<string, ServiceEnvironment> environmentsMap = new();
                 foreach (ServiceEnvironment environment1 in (ServiceEnvironment[])Enum.GetValues(typeof(ServiceEnvironment)))
@@ -594,10 +594,21 @@ namespace TTNETAPI_Sample_Console_TTUncovered_MarketCreation
                     if (environment1 == ServiceEnvironment.NotSet) continue;
 
                     string name = GetDescription<ServiceEnvironment>(environment1);
-                    Console.WriteLine(name);
-
                     environmentsMap.Add(name, environment1);
+
+                    Console.Write(name);
+                    if (environment1 != ServiceEnvironment.DevPerf)
+                    {
+                        Console.Write(", ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("]");
+                    }
+
                 }
+
+                string env = Console.ReadLine();
 
                 if (environmentsMap.ContainsKey(env))
                 {
